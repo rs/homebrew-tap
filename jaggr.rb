9b367f43c096b220167ae2757157c27458308c25
Olivier Poitrey <rs@rhapsodyk.net>
2018-03-12 01:07:49 -0700

    Brew formula update for jaggr version 1.0.0

--- jaggr.rb
+++ jaggr.rb
@@ -3,7 +3,7 @@ class Jaggr < Formula
   homepage "https://github.com/rs/jaggr"
   url "https://github.com/rs/jaggr/releases/download/1.0.0/jaggr_1.0.0_darwin_amd64.tar.gz"
   version "1.0.0"
-  sha256 "7f04bee7a75b19c27e25857e0781077405d5b1dd56618f9e5b64315faafcb453"
+  sha256 "79ca06e55b7873f543a4a4d11e8f393e41e3a42e7434029df62efe0e16eb192d"
 
   def install
     bin.install "jaggr"
