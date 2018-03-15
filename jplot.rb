ba6f1b07148d076e6520542471e6539cbc844d30
Olivier Poitrey <rs@rhapsodyk.net>
2018-03-14 23:32:30 -0700

    Brew formula update for jplot version 1.0.1

--- jplot.rb
+++ jplot.rb
@@ -1,9 +1,9 @@
 class Jplot < Formula
   desc "iTerm2 expvar/JSON monitoring tool"
   homepage "https://github.com/rs/jplot"
-  url "https://github.com/rs/jplot/releases/download/1.0.0/jplot_1.0.0_darwin_amd64.tar.gz"
-  version "1.0.0"
-  sha256 "f44f532d153ea3b1373b382f7964591f487bb8ae09ca8fb70caf7d3ef8555141"
+  url "https://github.com/rs/jplot/releases/download/1.0.1/jplot_1.0.1_darwin_amd64.tar.gz"
+  version "1.0.1"
+  sha256 "dc27af46cd526592206dfdae8292cfd24b45254fc2c0febfd9eb7e445e4058f7"
 
   def install
     bin.install "jplot"
