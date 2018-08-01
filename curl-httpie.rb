3c6db5ca1c86285be6ed7dfae0637566a034c2b6
Olivier Poitrey <rs@rhapsodyk.net>
2018-08-01 12:17:36 -0700

    Brew formula update for curl-httpie version v0.0.2

--- curl-httpie.rb
+++ curl-httpie.rb
@@ -1,9 +1,11 @@
 class CurlHttpie < Formula
   desc "HTTPie for curl"
   homepage "https://github.com/rs/curl-httpie"
-  url "https://github.com/rs/curl-httpie/releases/download/v0.0.1/curl-httpie_0.0.1_darwin_amd64.tar.gz"
-  version "0.0.1"
-  sha256 "ae6e29187de2e735cbc473a5e94cebaa2560f0eb9a6e6485e01fff609f889e58"
+  url "https://github.com/rs/curl-httpie/releases/download/v0.0.2/curl-httpie_0.0.2_darwin_amd64.tar.gz"
+  version "0.0.2"
+  sha256 "271664332eb50a3b42467b576d29cfcfd40718f6b7c56fe12bf278430dc8b1f9"
+  
+  conflicts_with "httpie"
 
   def install
     bin.install "http"
