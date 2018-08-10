e09c11b0d3827e6e2595b899e72e4322a2d4ad0c
Olivier Poitrey <rs@rhapsodyk.net>
2018-08-09 18:25:01 -0700

    Brew formula update for dnstrace version v1.1.2

--- dnstrace.rb
+++ dnstrace.rb
@@ -1,9 +1,9 @@
 class Dnstrace < Formula
   desc "DNS resolution tracing tool"
   homepage "https://github.com/rs/dnstrace"
-  url "https://github.com/rs/dnstrace/releases/download/v1.1.1/dnstrace_1.1.1_darwin_amd64.tar.gz"
-  version "1.1.1"
-  sha256 "c303d392e510f988d40f79609d4fd98fa63fec36be67d28c98dcd5da9449f211"
+  url "https://github.com/rs/dnstrace/releases/download/v1.1.2/dnstrace_1.1.2_darwin_amd64.tar.gz"
+  version "1.1.2"
+  sha256 "1f8c7cf5f5cbd50ff386f721d4a79ea0ff65181c178792c4c41a2f97a5c705ca"
 
   def install
     bin.install "dnstrace"
