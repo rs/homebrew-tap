00abe592e02be2a5e10bc9cd915ec6698a6c94c2
Olivier Poitrey <rs@rhapsodyk.net>
2018-09-13 03:09:05 -0700

    Brew formula update for curlie version v1.2.0

--- curlie.rb
+++ curlie.rb
@@ -1,9 +1,9 @@
 class Curlie < Formula
   desc "The power of curl, the ease of use of httpie."
   homepage "https://github.com/rs/curlie"
-  url "https://github.com/rs/curlie/releases/download/v1.1.0/curlie_1.1.0_darwin_amd64.tar.gz"
-  version "1.1.0"
-  sha256 "15b52472539f013cd49015c1c6ed50e6fddb243d125434370581f616e7605814"
+  url "https://github.com/rs/curlie/releases/download/v1.2.0/curlie_1.2.0_darwin_amd64.tar.gz"
+  version "1.2.0"
+  sha256 "1361f2d8283aecfe4f9e18230e8f765ac9d1965393b6c3f0cedfff8f6e9083bc"
 
   def install
     bin.install "curlie"
